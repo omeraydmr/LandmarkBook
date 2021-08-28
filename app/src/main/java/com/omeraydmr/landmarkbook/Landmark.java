e44fd171bd69c188fb01cc8639e80b7a072f83c1
Ömer Aydemir <osilix1@gmail.com>
2021-08-28 14:54:13 +0300

    Modeling landmarks information.

---
+++ app/src/main/java/com/omeraydmr/landmarkbook/Landmark.java
@@ -0,0 +1,14 @@
+package com.omeraydmr.landmarkbook;
+
+public class Landmark {
+
+    String name;
+    String country;
+    int image;
+
+    public Landmark(String name, String country, int image) {
+        this.name = name;
+        this.country = country;
+        this.image = image;
+    }
+}
--- app/src/main/java/com/omeraydmr/landmarkbook/MainActivity.java
+++ app/src/main/java/com/omeraydmr/landmarkbook/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Landmark> landmarkArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        landmarkArrayList = new ArrayList<>();
+
+        //Data
+        Landmark ayasofya = new Landmark("Ayasofya", "Istanbul-Turkey", R.drawable.ayasofya);
+        Landmark galatatower = new Landmark("Galata Tower", "Istanbul-Turkey", R.drawable.galatatower);
+        Landmark haydarpasa = new Landmark("Haydarpasa", "Istanbul-Turkey", R.drawable.hadarpasa);
+        Landmark saracoglu = new Landmark("Saracoglu Staadium", "Istanbul-Turkey", R.drawable.saracoglu);
+
+        landmarkArrayList.add(ayasofya);
+        landmarkArrayList.add(galatatower);
+        landmarkArrayList.add(haydarpasa);
+        landmarkArrayList.add(saracoglu);
+
     }
 }
