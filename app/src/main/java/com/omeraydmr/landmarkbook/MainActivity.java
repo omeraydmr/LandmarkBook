a5c8ccfeaab165223966f7e62096b4141e84c841
Ömer Aydemir <osilix1@gmail.com>
2021-08-28 18:10:39 +0300

    Added ListView() for interface.

--- app/src/main/java/com/omeraydmr/landmarkbook/MainActivity.java
+++ app/src/main/java/com/omeraydmr/landmarkbook/MainActivity.java
@@ -1,20 +1,25 @@
 package com.omeraydmr.landmarkbook;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.omeraydmr.landmarkbook.databinding.ActivityDetailsBinding;
 import com.omeraydmr.landmarkbook.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Landmark> landmarkArrayList;
     private ActivityMainBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +34,20 @@ public class MainActivity extends AppCompatActivity {
         Landmark ayasofya = new Landmark("Ayasofya", "Istanbul-Turkey", R.drawable.ayasofya);
         Landmark galatatower = new Landmark("Galata Tower", "Istanbul-Turkey", R.drawable.galatatower);
         Landmark haydarpasa = new Landmark("Haydarpasa", "Istanbul-Turkey", R.drawable.hadarpasa);
-        Landmark saracoglu = new Landmark("Saracoglu Staadium", "Istanbul-Turkey", R.drawable.saracoglu);
+        Landmark saracoglu = new Landmark("Saracoglu Stadium", "Istanbul-Turkey", R.drawable.saracoglu);
 
         landmarkArrayList.add(ayasofya);
         landmarkArrayList.add(galatatower);
         landmarkArrayList.add(haydarpasa);
         landmarkArrayList.add(saracoglu);
+
+        //Adapter
+            //ListView
+                //Mapping
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                landmarkArrayList.stream().map(landmark -> landmark.name).collect(Collectors.toList())
+        );
+        binding.listView.setAdapter(arrayAdapter);
 
     }
 }
